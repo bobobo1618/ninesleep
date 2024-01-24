9cad62f87d25fe06ea0d7d502b7c2ef7f004c97b
bobobo1618 <bobobo1618@users.noreply.github.com>
2024-01-24 19:02:55 +0100

    Add code to listen for data uploads so that frank works even with the hosts override

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 #[macro_use]
 extern crate rocket;
 
+use cbor::{Encoder, ToCbor};
+use log::{info, trace};
 use rocket::State;
+use rustc_serialize::{json::Json, Encodable};
 use std::{
     io::{Read, Write},
+    net::TcpListener,
     os::unix::net::{UnixListener, UnixStream},
-    sync::RwLock,
+    sync::{Arc, RwLock},
+    thread,
     time::Duration,
 };
-use cbor::{Encoder, ToCbor};
-use rustc_serialize::{json::Json, Encodable};
 
 // Just returns "ok" to show that communication with the firmware is working.
 #[get("/hello")]
-fn index(streamobj: &State<RwLock<UnixStream>>) -> String {
-    let mut stream = streamobj.write().unwrap();
+fn index(streamobj: &State<Arc<RwLock<Option<UnixStream>>>>) -> String {
+    if streamobj.read().unwrap().is_none() {
+        return "not connected".to_string();
+    }
+    let mut streamoption = streamobj.write().unwrap();
+    let stream = streamoption.as_mut().unwrap();
     let _ = stream.write(b"0\n\n");
     let _ = stream.set_read_timeout(Some(Duration::new(0, 50000000)));
     let mut result = String::new();
@@ -34,8 +41,12 @@ fn index(streamobj: &State<RwLock<UnixStream>>) -> String {
 // priming = false
 // settings = "BF61760162676C190190626772190190626C6200FF"
 #[get("/variables")]
-fn variables(streamobj: &State<RwLock<UnixStream>>) -> String {
-    let mut stream = streamobj.write().unwrap();
+fn variables(streamobj: &State<Arc<RwLock<Option<UnixStream>>>>) -> String {
+    if streamobj.read().unwrap().is_none() {
+        return "not connected".to_string();
+    }
+    let mut streamoption = streamobj.write().unwrap();
+    let stream = streamoption.as_mut().unwrap();
     let _ = stream.write(b"14\n\n");
     let _ = stream.set_read_timeout(Some(Duration::new(0, 50000000)));
     let mut result = String::new();
@@ -50,7 +61,7 @@ fn variables(streamobj: &State<RwLock<UnixStream>>) -> String {
 // tt: Timestamp in unix epoch for alarm
 // Presumably thermal alarm is controlled with the temperature commands
 #[post("/alarm/<side>", data = "<data>")]
-fn alarm(side: &str, data: &str, streamobj: &State<RwLock<UnixStream>>) -> String {
+fn alarm(side: &str, data: &str, streamobj: &State<Arc<RwLock<Option<UnixStream>>>>) -> String {
     let command = match side {
         "left" => 5,
         "right" => 6,
@@ -65,7 +76,11 @@ fn alarm(side: &str, data: &str, streamobj: &State<RwLock<UnixStream>>) -> Strin
     cbordata.encode(&mut cborencoder).unwrap();
     let serializeddata = hex::encode(cborencoder.as_bytes());
 
-    let mut stream = streamobj.write().unwrap();
+    if streamobj.read().unwrap().is_none() {
+        return "not connected".to_string();
+    }
+    let mut streamoption = streamobj.write().unwrap();
+    let stream = streamoption.as_mut().unwrap();
     let _ = stream.write(format!("{}\n{}\n\n", command, serializeddata).as_bytes());
     let _ = stream.set_read_timeout(Some(Duration::new(0, 50000000)));
     let mut result = String::new();
@@ -74,8 +89,12 @@ fn alarm(side: &str, data: &str, streamobj: &State<RwLock<UnixStream>>) -> Strin
 }
 
 #[post("/alarm-clear")]
-fn alarm_clear(streamobj: &State<RwLock<UnixStream>>) -> String {
-    let mut stream = streamobj.write().unwrap();
+fn alarm_clear(streamobj: &State<Arc<RwLock<Option<UnixStream>>>>) -> String {
+    if streamobj.read().unwrap().is_none() {
+        return "not connected".to_string();
+    }
+    let mut streamoption = streamobj.write().unwrap();
+    let stream = streamoption.as_mut().unwrap();
     let _ = stream.write(b"16\n\n");
     let _ = stream.set_read_timeout(Some(Duration::new(0, 50000000)));
     let mut result = String::new();
@@ -85,14 +104,18 @@ fn alarm_clear(streamobj: &State<RwLock<UnixStream>>) -> String {
 
 // Example CBOR: a1626c6200, a1626c621837. Controls light intensity.
 #[post("/settings", data = "<data>")]
-fn settings(data: &str, streamobj: &State<RwLock<UnixStream>>) -> String {
+fn settings(data: &str, streamobj: &State<Arc<RwLock<Option<UnixStream>>>>) -> String {
     let jsondata = Json::from_str(data).unwrap();
     let cbordata = jsondata.to_cbor();
     let mut cborencoder = Encoder::from_memory();
     cbordata.encode(&mut cborencoder).unwrap();
     let serializeddata = hex::encode(cborencoder.as_bytes());
 
-    let mut stream = streamobj.write().unwrap();
+    if streamobj.read().unwrap().is_none() {
+        return "not connected".to_string();
+    }
+    let mut streamoption = streamobj.write().unwrap();
+    let stream = streamoption.as_mut().unwrap();
     let _ = stream.write(format!("8\n{}\n\n", serializeddata).as_bytes());
     let _ = stream.set_read_timeout(Some(Duration::new(0, 50000000)));
     let mut result = String::new();
@@ -102,7 +125,11 @@ fn settings(data: &str, streamobj: &State<RwLock<UnixStream>>) -> String {
 
 // Takes an integer number of seconds, presumably until the heat ends, e.g. 7200.
 #[post("/temperature-duration/<side>", data = "<data>")]
-fn temperature_duration(side: &str, data: &str, streamobj: &State<RwLock<UnixStream>>) -> String {
+fn temperature_duration(
+    side: &str,
+    data: &str,
+    streamobj: &State<Arc<RwLock<Option<UnixStream>>>>,
+) -> String {
     let command = match side {
         "left" => 9,
         "right" => 10,
@@ -111,7 +138,11 @@ fn temperature_duration(side: &str, data: &str, streamobj: &State<RwLock<UnixStr
         }
     };
 
-    let mut stream = streamobj.write().unwrap();
+    if streamobj.read().unwrap().is_none() {
+        return "not connected".to_string();
+    }
+    let mut streamoption = streamobj.write().unwrap();
+    let stream = streamoption.as_mut().unwrap();
     let _ = stream.write(format!("{}\n{}\n\n", command, data).as_bytes());
     let _ = stream.set_read_timeout(Some(Duration::new(0, 50000000)));
     let mut result = String::new();
@@ -121,7 +152,11 @@ fn temperature_duration(side: &str, data: &str, streamobj: &State<RwLock<UnixStr
 
 // Takes a signed integer number. May represent tenths of degrees of heating/cooling. e.g. -40 = -4°C.
 #[post("/temperature/<side>", data = "<data>")]
-fn temperature(side: &str, data: &str, streamobj: &State<RwLock<UnixStream>>) -> String {
+fn temperature(
+    side: &str,
+    data: &str,
+    streamobj: &State<Arc<RwLock<Option<UnixStream>>>>,
+) -> String {
     let command = match side {
         "left" => 11,
         "right" => 12,
@@ -130,7 +165,11 @@ fn temperature(side: &str, data: &str, streamobj: &State<RwLock<UnixStream>>) ->
         }
     };
 
-    let mut stream = streamobj.write().unwrap();
+    if streamobj.read().unwrap().is_none() {
+        return "not connected".to_string();
+    }
+    let mut streamoption = streamobj.write().unwrap();
+    let stream = streamoption.as_mut().unwrap();
     let _ = stream.write(format!("{}\n{}\n\n", command, data).as_bytes());
     let _ = stream.set_read_timeout(Some(Duration::new(0, 50000000)));
     let mut result = String::new();
@@ -140,8 +179,12 @@ fn temperature(side: &str, data: &str, streamobj: &State<RwLock<UnixStream>>) ->
 
 // Takes a boolean string. Unclear what true/false mean exactly, maybe on/off?
 #[post("/prime")]
-fn prime(streamobj: &State<RwLock<UnixStream>>) -> String {
-    let mut stream = streamobj.write().unwrap();
+fn prime(streamobj: &State<Arc<RwLock<Option<UnixStream>>>>) -> String {
+    if streamobj.read().unwrap().is_none() {
+        return "not connected".to_string();
+    }
+    let mut streamoption = streamobj.write().unwrap();
+    let stream = streamoption.as_mut().unwrap();
     let _ = stream.write(b"13\n\n");
     let _ = stream.set_read_timeout(Some(Duration::new(0, 50000000)));
     let mut result = String::new();
@@ -151,24 +194,80 @@ fn prime(streamobj: &State<RwLock<UnixStream>>) -> String {
 
 #[rocket::main]
 async fn main() {
-    let listener = match UnixListener::bind("/deviceinfo/dac.sock") {
-        Ok(listener) => listener,
-        Err(error) => {
-            panic!("Failed to listen {:?}", error)
-        }
-    };
+    env_logger::init();
 
-    let stream = match listener.incoming().next() {
-        Some(val) => match val {
-            Ok(stream) => stream,
+    let stream = Arc::new(RwLock::<Option<UnixStream>>::new(None));
+
+    let streamcopy = stream.clone();
+    thread::spawn(move || {
+        let listener = match UnixListener::bind("/deviceinfo/dac.sock") {
+            Ok(listener) => listener,
             Err(error) => {
-                panic!("Failed to get connection {:?}", error)
+                panic!("Failed to listen {:?}", error)
             }
-        },
-        None => {
-            panic!("Failed to get connection")
+        };
+        for newstream in listener.incoming() {
+            match newstream {
+                Ok(newstream) => {
+                    info!("New UNIX socket connection");
+                    let _ = streamcopy.write().unwrap().insert(newstream);
+                }
+                Err(_) => continue,
+            }
         }
-    };
+    });
+
+    thread::spawn(|| {
+        let listener = TcpListener::bind("127.0.0.1:1337").unwrap();
+        for ostream in listener.incoming() {
+            match ostream {
+                Err(_) => continue,
+                Ok(mut stream) => {
+                    info!("Incoming TCP connection");
+                    let _ = stream.set_read_timeout(Some(Duration::new(60, 0)));
+                    let mut buf: [u8; 4096] = [0; 4096];
+                    let mut id = Vec::<u8>::new();
+                    loop {
+                        match stream.read(&mut buf) {
+                            Ok(size) => {
+                                if size == 0 {
+                                    info!("Connection closed");
+                                    break;
+                                }
+                                // info!("Received {} TCP bytes.", size);
+                                if size < 259 {
+                                    trace!("{}", hex::encode(&buf[0..size]));
+                                }
+                                if size == 80 && buf[0] == 0xA4 {
+                                    info!("Start of data sending");
+                                    let _ = stream.write(b"\xA2eprotocrawdpartgsession");
+                                }
+                                if size == 108 && buf[0] == 0xA4 {
+                                    id.copy_from_slice(&buf[size - 11..size - 8]);
+                                    info!("Incoming file stream {:02X?}", &id);
+                                }
+                                let endmarker = size > 8
+                                    && &buf[size - 8..size - 3] == b"ddata"
+                                    && buf[size - 1] == 0xFF;
+                                if endmarker || (size == 1 && buf[0] == 0xFF) {
+                                    info!("End of data sending for id {:02X?}", &id);
+                                    let mut outbuf: [u8; 28] = [0; 28];
+                                    outbuf[0..25].copy_from_slice(b"\xA3eprotocrawdpartebatchbid");
+                                    if id.len() == 3 {
+                                        outbuf[25..28].copy_from_slice(&id);
+                                    } else {
+                                        outbuf[25..28].copy_from_slice(&[0x19, 0x3c, 0x34]);
+                                    }
+                                    let _ = stream.write(&outbuf);
+                                }
+                            }
+                            Err(_) => break,
+                        }
+                    }
+                }
+            }
+        }
+    });
 
     println!("Client connected, starting rocket.");
 
@@ -186,7 +285,7 @@ async fn main() {
                 prime
             ],
         )
-        .manage(RwLock::new(stream))
+        .manage(stream)
         .launch()
         .await
     {
